aa09805ae8b0ed2ffa442085eec319858f59b0d2
pedrohlelis <pedrohenri.lelis@gmail.com>
2024-04-08 21:09:50 -0300

    commit pre checkout

--- obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
+++ obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bf9fae6b796edd7c159626977b8804a1124d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b17107b6047008a7be4636bb6b9c905225ef4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
