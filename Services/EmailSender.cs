1026147af8f5d0089b61a9d84fef43ebba5ce5a3
pedrohlelis <pedrohenri.lelis@gmail.com>
2024-05-08 16:41:12 -0300

    email google funcionando

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -12,22 +12,7 @@ namespace Glimpse.Services
     {
         public async Task SendEmailAsync(string Mailsubject, string To, string username, string message, string callbackUrl)
         {
-            // try{
-            // //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
-            // var apiKey = "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";
-            // var client = new SendGridClient(apiKey);
-            // var from = new EmailAddress("glimpse.emailsender@gmail.com", "Glimpse");
-            // var subject = Mailsubject;
-            // var to = new EmailAddress(To, username);
-            // var plainTextContent = message;
-            // var htmlContent = $"Please confirm your account by clicking <a href='{callbackUrl}'>here</a>.";
-            // var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            // await client.SendEmailAsync(msg);
-            // }
-            // catch(Exception e)
-            // {
-            //     Console.WriteLine(e.Message);
-            // }
+
         }
     }
 }
