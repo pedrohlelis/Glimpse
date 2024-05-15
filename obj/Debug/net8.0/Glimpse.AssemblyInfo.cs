28038110cdad8f2fcd3d66908310db4c33513a65
victorhtanaka <victortanakase@gmail.com>
2024-05-15 14:45:48 -0300

    updated css and html elements

--- obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
+++ obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f179238abddb6277bafaebe751c96564ef19b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798d9319f0a0f991a2e2ff372c1f3cb8edb99195")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
