cbb38335c8b62e468fa5be74e0b7922f4ccfa8ee
DevMatheus2 <patumcam@gmail.com>
2024-05-15 21:32:31 -0300

    Push to branch-matheus

--- obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
+++ obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
@@ -10,12 +10,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7393b4b6-9f43-4204-95ca-940dabd10082")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58e36ac7ff3805e3484865e49768c4b51b4bb3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
