08681fcb73d8976c714803e900e41726d1e9757d
pedrohlelis <pedrohenri.lelis@gmail.com>
2024-05-28 07:52:32 -0300

    version fix

--- obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
+++ obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e39b5d2587286cea06a5d93c480b295e1d59f55f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aaf0348bea685a51cd7e5c443b7ed4176573a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
