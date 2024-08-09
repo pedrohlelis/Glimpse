aaaa290a45a3da8e0f9cff762ad1891105eef1c1
victorhtanaka <victorhtanaka555@gmail.com>
2024-08-08 22:25:55 -0300

    a

--- wwwroot/js/board-info.js
+++ wwwroot/js/board-info.js
@@ -755,7 +755,6 @@ const addLaneButton = document.querySelector('.new-lane-btn');
 const laneForm = document.querySelector('.create-lane-form');
 const laneInput = document.querySelector('.lane-input');
 const cardForms = document.querySelectorAll('.create-card-form');
-const cardForms = document.querySelectorAll('.create-card-form');
 
 addLaneButton.addEventListener('click', function(event) {
     event.stopPropagation();
