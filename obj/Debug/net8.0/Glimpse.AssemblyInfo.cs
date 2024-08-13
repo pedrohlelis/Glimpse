9dc15d97769278dd7f7b33d96617c89c7c82826c
victorhtanaka <victortanakase@gmail.com>
2024-08-13 09:00:15 -0300

    default temp files

--- obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
+++ obj/Debug/net8.0/Glimpse.AssemblyInfo.cs
@@ -10,11 +10,12 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7393b4b6-9f43-4204-95ca-940dabd10082")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd045dd0c2eabfd94f80b4268460272af043e130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8561c1e9eef76cb4f0e5dfb0fccde4950e6629f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glimpse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
